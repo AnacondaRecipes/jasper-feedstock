54c9c6caf5e0afb49e319f691c977a85303a6e40
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-19 18:06:56 +0300

    jasper 4.2.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+set -eo pipefail
+
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*linux.* ]]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
 mkdir build_shared && cd $_
-cmake \
+cmake ${CMAKE_ARGS} \
+    -DALLOW_IN_SOURCE_BUILD=ON \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DJAS_STDC_VERSION="201112L" \
     -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=False \
-    ${CMAKE_PLATFORM_FLAGS[@]} ..
-make
+    "${CMAKE_PLATFORM_FLAGS[@]}" ..
+make -j${CPU_COUNT}
 make install
 cd ..
 
 mkdir build_static && cd $_
-cmake \
+cmake ${CMAKE_ARGS} \
+    -DALLOW_IN_SOURCE_BUILD=ON \
     -DJAS_ENABLE_SHARED=OFF \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DJAS_STDC_VERSION="201112L" \
     -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=False \
-    ${CMAKE_PLATFORM_FLAGS[@]} ..
-make
+    "${CMAKE_PLATFORM_FLAGS[@]}" ..
+make -j${CPU_COUNT}
 make install
 cd ..
