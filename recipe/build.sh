f9484905e103c92f817b1fb9256c61711d24cb8b
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-23 17:00:46 +0300

    Remove static build as it's overwritten by shared build

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,16 +18,3 @@ cmake ${CMAKE_ARGS} \
 make -j${CPU_COUNT}
 make install
 cd ..
-
-mkdir build_static && cd $_
-cmake ${CMAKE_ARGS} \
-    -DALLOW_IN_SOURCE_BUILD=ON \
-    -DJAS_ENABLE_SHARED=OFF \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DJAS_STDC_VERSION="201112L" \
-    -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=False \
-    "${CMAKE_PLATFORM_FLAGS[@]}" ..
-make -j${CPU_COUNT}
-make install
-cd ..
