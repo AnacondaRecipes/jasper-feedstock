9e514033ef5b4cf826919ff9b161932f8802b0f5
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-25 13:48:02 +0300

    Remove build static on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,20 +18,3 @@ cmake -G "Ninja" \
 cmake --build . --config Release --target=install --parallel ${CPU_COUNT}
 ctest --output-on-failure || true
 cd ..
-
-if [[ $(uname) == Darwin ]]; then
-    mkdir build_static && cd $_
-    cmake -G "Ninja" \
-        ${CMAKE_ARGS} \
-        -DALLOW_IN_SOURCE_BUILD=ON \
-        -DCMAKE_PREFIX_PATH=$PREFIX \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
-        -DJAS_ENABLE_LIBHEIF=OFF \
-        -DJAS_ENABLE_DOC=OFF \
-        "${CMAKE_PLATFORM_FLAGS[@]}" ..
-
-    cmake --build . --config Release --target=install --parallel ${CPU_COUNT}
-    ctest --output-on-failure || true
-    cd ..
-fi
