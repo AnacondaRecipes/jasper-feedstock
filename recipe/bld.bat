0d18ff81f9e8ec817924492a6e94a13a091a2688
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-26 17:50:06 +0300

    Enable debug mode, add pkg-config, pin cmake to 3.22

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,12 +2,16 @@
 set CC=cl.exe
 set CXX=cl.exe
 
-dir  /s %LIBRARY_INC%
+set PKG_CONFIG_PATH="%LIBRARY_BIN%\pkgconfig;%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig;%BUILD_PREFIX%\Library\bin\pkgconfig"
+set PKG_CONFIG_EXECUTABLE=%LIBRARY_BIN%\pkg-config
+
+copy "%LIBRARY_LIB%\pkgconfig\freeglut.pc" "%LIBRARY_LIB%\pkgconfig\glut.pc"
+copy "%LIBRARY_INC%\GL\glut.h" "%LIBRARY_INC%\GL\freeglut.h"
 
 mkdir build_src
 cd build_src
 
-cmake ^
+cmake -LAH ^
     -G "Ninja" ^
     %CMAKE_ARGS% ^
     -DALLOW_IN_SOURCE_BUILD=ON ^
@@ -18,7 +22,10 @@ cmake ^
     -DJAS_ENABLE_LIBHEIF=OFF ^
     -DJAS_ENABLE_DOC=OFF ^
     -DJPEG_LIBRARY_RELEASE=%LIBRARY_LIB%\libjpeg.lib ^
-    -DGLUT_INCLUDE_DIR=%LIBRARY_INC% ^
+    -DGLUT_INCLUDE_DIR:PATH=%LIBRARY_INC% ^
+    -DGLUT_glut_LIBRARY_RELEASE=%LIBRARY_LIB%\freeglut.lib ^
+    -Dpkgcfg_lib_PC_GLUT_glut:FILEPATH="%LIBRARY_LIB%\pkgconfig\glut.pc" ^
+    -DCMAKE_FIND_DEBUG_MODE=TRUE ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
