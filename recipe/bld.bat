c7780ad96cafd0d145e0776afe8e5b0188a981f6
Serhii Kupriienko <skupriienko@anaconda.com>
2024-05-01 12:53:05 +0300

    Fix missing freeglut's header and library

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,17 @@
 set CC=cl.exe
 set CXX=cl.exe
 
+REM The pkg-config path and executable are needed to find the freeglut header and library
 set PKG_CONFIG_PATH="%LIBRARY_BIN%\pkgconfig;%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig;%BUILD_PREFIX%\Library\bin\pkgconfig"
 set PKG_CONFIG_EXECUTABLE=%LIBRARY_BIN%\pkg-config
 
-copy "%LIBRARY_LIB%\pkgconfig\freeglut.pc" "%LIBRARY_LIB%\pkgconfig\glut.pc"
+REM The default %LIBRARY_LIB%\freeglut.lib can't be found by CMake even is DGLUT_glut_LIBRARY_RELEASE is defined.
+REM While linking C executable src\app\jiv.exe two files are required glut.lib AND(!) freeglutd.lib instead of freeglut.lib. 
+REM So we rename freeglut.lib to glut.lib and freeglutd.lib.
+REM At the same time pkg-config is looking for glut.pc but not freeglut.pc. Renaming it fixes the issue.
+copy %LIBRARY_LIB%\pkgconfig\freeglut.pc %LIBRARY_LIB%\pkgconfig\glut.pc
+copy %LIBRARY_LIB%\freeglut.lib %LIBRARY_LIB%\glut.lib
+copy %LIBRARY_LIB%\freeglut.lib %LIBRARY_LIB%\freeglutd.lib
 
 mkdir build_src
 cd build_src
@@ -22,9 +29,7 @@ cmake -LAH ^
     -DJAS_ENABLE_DOC=OFF ^
     -DJPEG_LIBRARY_RELEASE=%LIBRARY_LIB%\libjpeg.lib ^
     -DGLUT_INCLUDE_DIR:PATH=%LIBRARY_INC% ^
-    -DGLUT_glut_LIBRARY_RELEASE=%LIBRARY_LIB%\freeglut.lib ^
-    -Dpkgcfg_lib_PC_GLUT_glut:FILEPATH="%LIBRARY_LIB%\pkgconfig\freeglut.pc" ^
-    -DCMAKE_FIND_DEBUG_MODE=TRUE ^
+    -DGLUT_glut_LIBRARY_RELEASE=%LIBRARY_LIB%\glut.lib ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
